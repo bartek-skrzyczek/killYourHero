fb5bc6930ca5072e40bb14caef789d7995b9818b
BARTLOMIEJ SKRZYCZEK <you@example.combartek.skrzyczek@gmail.com>
2020-09-19 14:12:34 +0200

    hiding intro content when game started

--- hero.js
+++ hero.js
@@ -1,8 +1,8 @@
 const gridSquares = document.createDocumentFragment();
+const grid = document.querySelector(".grid");
 const heros = document.querySelectorAll(".hero");
 const timeLeft = document.querySelector("#time-left");
 const mainContent = document.querySelector(".main-content");
-let score = document.querySelector("#score");
 let result = 0;
 
 const images = [
@@ -10,12 +10,6 @@ const images = [
     '<img class="hero" id="hero2" src="images/pig.svg">',
     '<img class="hero" id="hero3" src="images/zoology.svg">'
 ];
-
-const backgroundImages = {
-    hero1:'images/lion.svg',
-    hero2:'images/pig.svg',
-    hero3:'images/zoology.svg',
-}
 
 function introduction() {
     let introContent = document.createElement('div');
@@ -42,90 +36,114 @@ function introduction() {
 
             el = document.getElementById('start');
             el.addEventListener("click",start,false);
-        
-                function start()
-            {
-                runGame();
-                console.log(el);
-                //el = document.getElementById('start');
-                //el.style.backgroundImage='url('+selectedHero+')';
-
-
-                for(let i=0; i<16; i++) {
-                    let newDiv = document.createElement('div');
-                    newDiv.id = i;
-                    newDiv.className = "square";
-                    gridSquares.appendChild(newDiv);
-                }
-                
-                document.getElementsByClassName("grid")[0].appendChild(gridSquares);
-                
-                const square = document.querySelectorAll(".square");
-
-
-
-                
-                
-                function randomSquare() {
-                    const qqq = document.querySelectorAll('.target');
-                    const styles = {
-                        backgroundImage: 'url('+selectedHero+')'
-                    };
-
-                    qqq[0].style.backgroundImage='url('+selectedHero+')';
-
-                    square.forEach(className => {
-                        className.classList.remove("target");
-                        //target.style.backgroundImage='none';
-                        
-                    })
-                    let randomPosition = square[Math.floor(Math.random() * 16)];
-                    randomPosition.classList.add("target");
-
-                    //target[0].style.backgroundImage='url('+selectedHero+')';
-
-                    
-
-                        
-                        //target[i].style.backgroundImage='url('+selectedHero+')';
-                        //target[i].style.backgroundImage='';
-
-                    hitPosition = randomPosition.id;
-                }
-                
-                square.forEach(id => {
-                    id.addEventListener('mouseup', () => {
-                        
-                        if(id.id === hitPosition) {
-                            result = result + 1;
-                            score.textContent = result;
-                        }
-                    })
-                })
-                
-                function runGame() {
-                    let timerId = null;
-                    timerId = setInterval(randomSquare, 4000)
-                }
-            }
+           
         })
-
-
     })
 }
 
+function hideIntro() {
+    mainContent.innerHTML = '';
+}
+
+function start() {
+
+    hideIntro();
+    runGame();
+    
+    for(let i=0; i<16; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.id = i;
+        newDiv.className = "square";
+        gridSquares.appendChild(newDiv);
+    }
+    
+    grid.appendChild(gridSquares);
+    const square = document.querySelectorAll(".square");
+
+    let ScoreHeader = document.createElement('h2');
+    ScoreHeader.textContent = 'Your score';
+    mainContent.appendChild(ScoreHeader);
+    
+    let scoreText = document.createElement('div');
+    scoreText.id = 'score';
+    let score = document.querySelector('#score');
+    console.log(scoreText);
+    mainContent.appendChild(scoreText);
 
 
+    function randomSquare() {
+        let randomPosition = square[Math.floor(Math.random() * 16)];
+        square.forEach(className => {
+            className.classList.remove("target");
+            for (let i=0; i<square.length; i++) {
+                square[i].style.backgroundImage='none';
+            }
+        })
 
+        randomPosition.classList.add("target");
+        randomPosition.style.backgroundImage='url('+selectedHero+')';
+        hitPosition = randomPosition.id;
+
+        square.forEach(id => {
+            id.addEventListener('mouseup', () => {
+                if(id.id === hitPosition) {
+                    result = result + 1;
+                    scoreText.textContent = result;
+                }
+            })
+        })
+    }
+    
+    function runGame() {
+        function startTimer(duration, display) {
+            let timer = duration, minutes, seconds;
+            const interval = setInterval(function () {
+                minutes = parseInt(timer / 60, 10)
+                seconds = parseInt(timer % 60, 10);
+        
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+        
+                display.textContent = minutes + ":" + seconds;
+                //console.log(timer);
+                
+
+                if (timer === 0) {
+                    clearInterval(interval);
+                    endGame();
+                }
+                timer--;
+               
+            }, 1000);
+
+        }
+
+        let timerId;
+        timerId = setInterval(randomSquare, 1000);
+        let startingTime = 60 * 0.1;
+
+        let newDiv = document.createElement('div');
+        let newHeader = document.createElement('h2');
+        newHeader.textContent = "Time left";
+        newDiv.id = 'time-left';
+
+        mainContent.appendChild(newHeader);
+        mainContent.appendChild(newDiv);
+
+        let display = document.querySelector('#time-left');
+        startTimer(startingTime, display);
+    }
+
+    function endGame() {
+        grid.textContent = "";
+    }
+}
 
 function countDown() {
     let currentTimeLeft = 60000;
-    //currentTime--;
-    //timeLeft.textContent = currentTime;
 }
 
 
-let timerId = setInterval(countDown, 4000);
+let timerId = setInterval(countDown, 10000);
 
 introduction();
-//
